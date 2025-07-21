54fb5cdad1ea1be01e4c9e7c36065da919a20a32
vanessa wanjiru <vanessawanjiru2023@gmail.com>
2025-07-21 18:03:36 +0300

    Update question2.py

---
+++ question2.py
@@ -0,0 +1,9 @@
+# Arithmetic sequence generator
+start = 5
+difference = 3
+terms = 8
+
+# Generate and print the sequence
+for i in range(terms):
+    term = start + i * difference
+    print(term)
